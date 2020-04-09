b98ceaaa9a5d999e57203788985a4766ad20111b
tanakashun <michael021212@gmail.com>
2020-04-09 15:48:07 +0900

    jobリストアプリに変更

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './Api' // './App'でjobリストアプリ、'./Api'でwikipediaApiの切り替え
+import App from './App' // './App'でjobリストアプリ、'./Api'でwikipediaApiの切り替え
 import './index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
